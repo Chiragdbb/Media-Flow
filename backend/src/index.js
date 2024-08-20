8adf480453299e709d671fc48c5098d05dd24fbd
Chiragdbb <schirag414@gmail.com>
2024-08-20 17:55:24 +0530

    Add: Connect to DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-console.log("first")
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.on("error", (e) => {
+            console.log("ERR: ", e);
+            throw e;
+        });
+
+        app.listen(port, () => {
+            console.log(` Server is running on port: ${port}`);
+        });
+    })
+    .catch((e) => {
+        console.log("MongoDB connection failed !!!", e);
+    });
