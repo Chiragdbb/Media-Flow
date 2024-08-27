568f6b7b064a8107de636d02959bd56b99380a12
Chiragdbb <schirag414@gmail.com>
2024-08-28 05:24:35 +0530

    Add: healthcheck controller and routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,9 +17,11 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // routes import
-import userRouter from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
+import healthcheck from "./controllers/healthcheck.controller.js";
 
 // routes declaration
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/heathcheck", healthcheck);
 
 export default app;
